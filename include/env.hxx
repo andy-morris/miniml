21416f77eba671778097a582455fc4df2c85f497
Andy Morris <andy@adradh.org.uk>
2015-01-19 22:18:33 +0100

    Mapped environments

--- include/env.hxx
+++ include/env.hxx
@@ -2,19 +2,29 @@
 #define ENV_HXX_V1MWKBLN
 
 #include "id.hxx"
+#include "ptr.hxx"
+#include <functional>
 #include <unordered_map>
 
 namespace miniml
 {
 
 template <typename T>
-class Env
+class EnvBase
 {
 public:
-  Env(const Ptr<Env> next): m_next(next) {}
+  virtual ~EnvBase() {}
+  virtual Ptr<T> lookup(const Id&) const = 0;
+};
+
+template <typename T>
+class Env final: public EnvBase<T>
+{
+public:
+  Env(const Ptr<EnvBase<T>> next): m_next(next) {}
   Env(): Env(nullptr) {}
 
-  Ptr<T> lookup(const Id&) const;
+  virtual Ptr<T> lookup(const Id&) const override;
 
   void insert(const Id&, const Ptr<T>);
 
@@ -29,7 +39,7 @@ public:
 
 private:
   std::unordered_map<Id, Ptr<T>, std::hash<Id>> m_elements;
-  const Ptr<Env> m_next;
+  const Ptr<EnvBase<T>> m_next;
 };
 
 
@@ -50,6 +60,40 @@ void Env<T>::insert(const Id &id, const Ptr<T> val)
   m_elements.insert(std::make_pair(id, val));
 }
 
+
+template <typename T, typename U>
+class MappedEnv final: public EnvBase<T>
+{
+public:
+  /// Function from U to T but with more pointing.
+  using Fun = std::function<Ptr<T>(Ptr<U>)>;
+
+  MappedEnv(const Ptr<Env<U>> inner, const Fun func):
+    m_inner(inner), m_func(func)
+  {}
+
+  inline Ptr<Env<U>> inner() const { return m_inner; }
+  inline Ptr<T> map(Ptr<U> x) const { return m_func(x); }
+
+  virtual Ptr<T> lookup(const Id &id) const override;
+private:
+  const Ptr<Env<U>> m_inner;
+  const Fun m_func;
+};
+
+template <typename T, typename U>
+Ptr<T> MappedEnv<T, U>::lookup(const Id &id) const
+{
+  auto ret = inner()->lookup(id);
+  return ret? map(ret): nullptr;
+}
+
+template <typename T, typename U>
+Ptr<Env<T>> map_env(const Ptr<Env<U>> inner,
+                    typename MappedEnv<T, U>::Fun func)
+{
+  return ptr<Env<T>>(ptr<MappedEnv<T, U>>(inner, func));
+}
 
 }
 
